1d0e16aa98cef9a29b31c4bafb14cb86d045424f
juedu36 <120651477+devJuliaLeal@users.noreply.github.com>
2023-03-05 16:58:25 -0300

    commit

--- meu_site.py
+++ meu_site.py
@@ -3,30 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-# criar a primeira página
-# route -> meusite.com/contatos
-# função -> o que você vai exibir naqula página
-
 @app.route("/")
 def homepage():
     return render_template("homepage.html")
 
-# cria outra página
 
-
-@app.route("/contatos")
-def contatos():
-    return render_template("contatos.html")
-
-
-@app.route("/usuarios/<nome_usuario>")
-def usuarios(nome_usuario):
-    return render_template("usuarios.html", nome_usuario=nome_usuario)
-
-
-# colocar o site no ar
-if __name__ == "__main__":
-    app.run(debug=True)
-
-
---
+app.run(debug=True)
